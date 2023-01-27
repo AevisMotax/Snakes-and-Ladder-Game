04d6478dcc3913a639136c7dd97acd2ef322299d
Aevismotax1 <108089675+AevisMotax@users.noreply.github.com>
2023-01-27 13:17:32 -0500

    Update PlayLadderAndSnake.java

--- PlayLadderAndSnake.java
+++ PlayLadderAndSnake.java
@@ -5,11 +5,11 @@
  * COMP 249 Section S - Winter 2023
  * 3 February 2023
  * --------------------------- */
-
-
-
-
-
+/* The driver file is where many things are being played.
+ * 
+ * 
+ * 
+ */
 import java.util.Scanner;
 
 public class PlayLadderAndSnake {
@@ -17,25 +17,18 @@ public class PlayLadderAndSnake {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner keyb = new Scanner(System.in);
-		
-		//Declare variables
-	/*	int y = 1;
-		int x = 11;
-		int [][] lol = new int[10][10];
-		
-		for (int i = 0; i < lol.length; i++) {
-			
-			for (int z = 0; z< lol[i].length; z++) {
-				lol[i][z] = y++;
-			}
-		} */
+		LadderAndSnake Game1 = new LadderAndSnake();
 		
 		System.out.println("Hello, welcome to the Snake And Ladder Game!");
-		System.out.println("To begin, initialize the amount of players:");
+		Game1.Rules();
+		Game1.play();
 		
-		System.out.println(LadderAndSnake.flipDice());
 		
 		keyb.close();	
 	}
 
 }
+
+
+
+
